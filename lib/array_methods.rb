a664ca04071aa41fe873bd9f0322395f3f9253ae
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-02 21:34:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,5 +10,10 @@ find_max_value([1,2,1,3,4,3,5,4,3,2,1])
 end
 
 def find_min_value(array)
-  find_min_value([10,5,3,7,19,1,3,10])
+  array = [10,5,3,7,19,1,3,10]
+  counter = 0
+  while array[counter] do
+    
+    counter += 1
+  end
 end
